dc508153f0f9ab69e7be6baced5d545f9ad49ce7
S4m0S <adamnasreddine@outlook.com>
2023-11-14 14:57:49 +0100

    lecture du fichier operation fini

--- test_file_Adam.c
+++ test_file_Adam.c
@@ -2,10 +2,9 @@
 
 
 int main(void){
-    printf("oui\n");
     operations_l * liste_operation;
 
-    int nb_operation = lecture_fichier_operation(liste_operation);
+    int nb_operation = lecture_fichier_operation(&liste_operation);
 
     for(int i = 0;i<nb_operation;i++)
         printf("Operation : %i, temps : %f\n",liste_operation[i].operation,liste_operation[i].temps);
