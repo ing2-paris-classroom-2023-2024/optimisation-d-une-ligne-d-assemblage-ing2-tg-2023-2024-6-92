a42a6fadf5ecce15f79275216ae233f426a6be1a
Michael ADDA <michael.adda92@gmail.com>
2023-11-18 14:44:52 +0100

    yes

--- test_Mike.c
+++ test_Mike.c
@@ -10,12 +10,13 @@ typedef struct sommet {
 typedef struct graphe {
     int *tabOperations;
     sommet *listeArc;
+    int taille;
 } graphe;
 
-graphe lireFichier(const char *nomFichier) {
+graphe *lireFichier(const char *nomFichier) {
     FILE *fichier = fopen(nomFichier, "r");
     if (fichier == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
+        printf("probleme fichier");
         exit(EXIT_FAILURE);
     }
 
@@ -23,7 +24,7 @@ graphe lireFichier(const char *nomFichier) {
     int maxDegre = 0;
     int sommet1, sommet2;
 
-    // Trouver le nombre de sommets et le degré maximum
+
     while (fscanf(fichier, "%d %d", &sommet1, &sommet2) == 2) {
         if (sommet1 > maxNom) {
             maxNom = sommet1;
@@ -37,42 +38,40 @@ graphe lireFichier(const char *nomFichier) {
         if (sommet2 > maxDegre) {
             maxDegre = sommet2;
         }
+        printf("%d,%d\n",sommet1,sommet2);
     }
 
-    // Réinitialiser le curseur de fichier
+
     fclose(fichier);
 
     fichier = fopen(nomFichier, "r");
     if (fichier == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
+        printf("probleme fichier");
         exit(EXIT_FAILURE);
     }
 
-    // Initialiser le graphe
-    graphe g;
-    g.tabOperations = malloc((maxDegre + 1) * sizeof(int));
-    g.listeArc = malloc((maxNom + 1) * sizeof(sommet));
 
-    for (int i = 0; i <= maxNom; i++) {
-        g.listeArc[i].nom = i;
-        g.listeArc[i].degre = 0;
-        g.listeArc[i].adjacents = NULL;
-    }
+    graphe *g=malloc(sizeof(graphe));
+    g->tabOperations = malloc((maxDegre + 1) * sizeof(int));
+    g->listeArc = malloc((maxNom + 1) * sizeof(sommet));
 
-    // Lire les arêtes et construire la liste d'adjacence
+
+
+
     while (fscanf(fichier, "%d %d", &sommet1, &sommet2) == 2) {
         // Mettre à jour le degré des sommets
-        g.listeArc[sommet1].degre++;
-        g.listeArc[sommet2].degre++;
+        g->listeArc[sommet1].degre++;
+        g->listeArc[sommet2].degre++;
+        g->taille+=1;
     }
 
-    // Allouer de la mémoire pour les listes d'adjacence
+
     for (int i = 0; i <= maxNom; i++) {
-        g.listeArc[i].adjacents = malloc(g.listeArc[i].degre * sizeof(int));
-        g.listeArc[i].degre = 0;  // Réinitialiser le degré pour la construction de la liste d'adjacence
+        g->listeArc[i].adjacents = malloc(g->listeArc[i].degre * sizeof(int));
+        g->listeArc[i].degre = 0;
     }
 
-    // Remplir les listes d'adjacence
+
     fclose(fichier);
     fichier = fopen(nomFichier, "r");
     if (fichier == NULL) {
@@ -81,8 +80,8 @@ graphe lireFichier(const char *nomFichier) {
     }
 
     while (fscanf(fichier, "%d %d", &sommet1, &sommet2) == 2) {
-        g.listeArc[sommet1].adjacents[g.listeArc[sommet1].degre++] = sommet2;
-        g.listeArc[sommet2].adjacents[g.listeArc[sommet2].degre++] = sommet1;
+        g->listeArc[sommet1].adjacents[g->listeArc[sommet1].degre++] = sommet2;
+        g->listeArc[sommet2].adjacents[g->listeArc[sommet2].degre++] = sommet1;
     }
 
     fclose(fichier);
@@ -90,16 +89,18 @@ graphe lireFichier(const char *nomFichier) {
 }
 
 int main() {
-    graphe g = lireFichier("exclusions.txt");
+    graphe *g = lireFichier("exclusions.txt");
 
-    // Utilisez les données du graphe comme nécessaire
 
-    // Libérez la mémoire à la fin du programme
-    free(g.tabOperations);
-    for (int i = 0; i < g.listeArc->nom; i++) {
-        free(g.listeArc[i].adjacents);
+    for (int i = 0; i <= g->taille; i++) {
+        printf("%d\n",g->listeArc[i].nom);
     }
-    free(g.listeArc);
+
+    free(g->tabOperations);
+    for (int i = 0; i < g->listeArc->nom; i++) {
+        free(g->listeArc[i].adjacents);
+    }
+    free(g->listeArc);
 
     return 0;
 }
