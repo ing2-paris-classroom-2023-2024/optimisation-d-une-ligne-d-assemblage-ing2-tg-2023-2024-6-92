6448b4ad22e2b7dd7eeb5f2830a297fd04d47d1e
S4m0S <adamnasreddine@outlook.com>
2023-11-20 12:13:55 +0100

    avancement lecture fichier dynamique

--- header.h
+++ header.h
@@ -7,6 +7,8 @@ typedef struct {
 }operations_l;
 
 int lecture_fichier_operation(operations_l** liste_operation);
+float lecture_temps_cycle(void);
+int nb_operation(void);
 
 // Ouverture du fichier operation pour lecture des valeurs et inclusions dans une liste operation_l 
 int lecture_fichier_operation(operations_l** liste_operation){
@@ -18,15 +20,44 @@ int lecture_fichier_operation(operations_l** liste_operation){
     }
     int operation_acutelle=0;
     float temps_actuelle=0;
-    int nombre_operation = 0;
+    int operation_actuelle= 0;
+    int nombre_operation = nb_operation();  // permet de gérer le fait qu'on ne connaise pas le nombre d'opération à l'avance
+    (*liste_operation) = malloc(sizeof(operations_l)*nombre_operation);
     while(fscanf(fichier_ope,"%i %f\n",&operation_acutelle,&temps_actuelle)==2){
-        nombre_operation++;
-        (*liste_operation) = realloc((*liste_operation),sizeof(operations_l)*nombre_operation);     // permet de gérer le fait qu'on ne connaise pas le nombre d'opération à l'avance 
-        (*liste_operation)[nombre_operation-1].operation = operation_acutelle;
-        (*liste_operation)[nombre_operation-1].temps = temps_actuelle;
+        operation_actuelle++;
+        (*liste_operation)[operation_actuelle-1].operation = operation_acutelle;
+        (*liste_operation)[operation_actuelle-1].temps = temps_actuelle;
         operation_acutelle = 0;
         temps_actuelle =0;
     }
-    
+    //fclose(fichier_ope);
     return nombre_operation;
+}
+
+float lecture_temps_cycle(void){
+    FILE* fichier_temps;
+    fichier_temps = fopen("temps_cycle.txt","r");
+    if(fichier_temps==NULL){
+        printf("Erreur lors de la lecture du fichier de temps de cycle");
+        return -1;
+    }
+    float temps_cycle=0;
+    fscanf(fichier_temps,"%f",&temps_cycle);
+    //fclose(fichier_temps);
+    return temps_cycle;  
+}
+
+int nb_operation(void){
+    FILE* fichier;
+    fichier = fopen("operation.txt","r");
+    int nombre= 0;
+    char c;
+    while((c=fgetc(fichier))!=EOF){
+        if(c=='\n')
+            nombre++;
+    }
+    nombre++;
+    fclose(fichier);
+    return nombre;
+
 }
