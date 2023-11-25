1b545a9d529a784ada5cb02e7e2188263f1805a9
kanai <kanaislouis@gmail.com>
2023-11-25 18:34:17 +0100

    début algorithme Pert

--- fichier_louis.c
+++ fichier_louis.c
@@ -1,95 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct maillon{
-    int note;
-    struct maillon* suiv;
-}t_maillon;
-
-
-t_maillon* ajouterElement(t_maillon* ancre, int note_a_ajouter ){
-    t_maillon *tmp = ancre;
-    t_maillon *nouveau_maillon = (t_maillon *)(sizeof (t_maillon));
-    nouveau_maillon->note= note_a_ajouter;
-    if(ancre == NULL){
-        return nouveau_maillon;
+void pert_algo(int nombre_operations, operations_l **liste_operations,float temps_cycle){ // ici on va essayer d'implementer la methode de Pert qui ressemble beaucoup à notre cas d'usage
+    int** liste_operations_effectuer = (int*) malloc(sizeof(int)*nombre_operations);     //liste des operations qui ont deja ete effectue par cycle, quand i = 0 alors c'est le premier cycle toutes les operations effectue
+    for(int i = 0;i<nombre_operations;i++){
+        liste_operations_effectuer[i] = (int*) malloc(sizeof(int)*nombre_operations);
     }
-    else{
-        while(tmp->suiv !=NULL){
-            tmp =tmp->suiv;
+    int * operations_effectuer = (int*) malloc(sizeof(int)*nombre_operations);           // cette liste nous permet d'acceder plus rapidement et plus facilement a toutes les operations effectuer sans avoir a faire un balayge en 2D
+    int* operations_facile = (int*) malloc(sizeof(int) * nombre_operations);            //liste des operations consideré comme facile à effectue car elle ne requiert aucunes autre tache pour etre effectue
+    int operations_trie_precedence = (int) malloc(sizeof(int)*nombre_operations);     // liste qui contient les operations trie par ordre croissant de nombre de precedances, plus une operation à besoin de precedences plus elle sera mise vers le fin de la liste
+    int* operations_possible = (int*) malloc(sizeof(int)*nombre_operations);            //liste des operations pouvants etre effectue à un instant T
+    int operations_facile_index = -1;
+    for(int operation_actuelle=0;operation_actuelle<nombre_operations;operation_actuelle++){
+        if((*liste_operations)[operation_actuelle].nb_operation_precedente==0)
+            operations_facile[operations_facile_index++] = (*liste_operations)[operation_actuelle].operation;
+
+    }
+
+
+
+}
+
+void tri_ordre_operation(tuple** operations_trie, operations_l** liste_operations,int nombre_operations){
+    tuple provisoire;
+    for(int i = 0;i<nombre_operations;i++){
+        (*operations_trie)[i].sommet = (*liste_operations)[i].operation;
+        (*operations_trie)[i].nb = (*liste_operations)[i].nb_operation_precedente;
+    }
+    for(int i = 0; i<nombre_operations;i++){
+        for(int j = 0;j<nombre_operations;j++){
+            if((*operations_trie)[j].nb>(*operations_trie)[j+1].nb){
+                provisoire.nb = (*operations_trie)[j].nb;
+                provisoire.sommet = (*operations_trie)[j].sommet;
+                (*operations_trie)[j].nb = (*operations_trie)[j+1].nb;
+                (*operations_trie)[j].sommet = (*operations_trie)[j+1].sommet;
+                (*operations_trie)[j+1].nb = provisoire.nb;
+                (*operations_trie)[j+1].sommet = provisoire.sommet;
+
+            }
         }
     }
-    return ancre;
 }
 
-t_maillon* depiler(t_maillon *ancre, t_maillon *maillon_depile){
-    if(ancre == NULL){
-        printf("la pile est déjà vide\n");
-        return  NULL;
+
+
+void update_operations_possible(int index_operation_facile,int **operation_possible, int nombre_operation, int * operation_effectuer, int * opeartions_facile, operations_l **liste_operation,float temps_cycle){  //cette fonction va nous permettre de mettre a jour notre liste contenant les operations effectuables à un instant T
+    int nombre_operation_possible = 0;
+    for(int i = 0;i<index_operation_facile;i++){
+        (*operation_possible)[i] = opeartions_facile[i];
+        nombre_operation_possible++;
     }
-    (*maillon_depile)  ;
-
-}
-typedef struct spectacle
-{
-    char * nom;
-    float temps;
-    struct spectacle * suivant;
-} t_spectacle;
-
-// Définition du type t_file
-typedef struct file
-{
-    t_spectacle * premier;
-    t_spectacle * dernier;
-} t_file;
-
-// 3.2 Prototype du sous-programme creerSpectacle
-t_spectacle* creerSpectacle();
-
-// 3.3 Implémentation du sous-programme creerSpectacle
-t_spectacle* creerSpectacle() {
-    t_spectacle * nouveau = (t_spectacle *) malloc(sizeof(t_spectacle));
-    nouveau->nom = malloc(100 * sizeof(char));
-
-    printf("Entrer le nom du spectacle : ");
-    scanf("%s", nouveau->nom);
-
-    printf("Entrer le temps du spectacle : ");
-    scanf("%f", &(nouveau->temps));
-
-    nouveau->suivant = NULL;
-
-    return nouveau;
-}
-
-// 3.4 Implémentation du sous-programme ajoutSpectacle
-void ajoutSpectacle( t_file * fileDyn, t_spectacle * nouveau){
-    if(fileDyn->premier == NULL){
-        fileDyn->premier = nouveau;
-        fileDyn->dernier = nouveau;
-    } else {
-        fileDyn->dernier->suivant = nouveau;
-        fileDyn->dernier = nouveau;
-    }
-}
-
-// 3.5 Sous-programme afficherSpectacle
-void afficherSpectacle(t_file * fileDyn) {
-    t_spectacle * current;
-    if(fileDyn->premier == NULL) {
-        printf("La file est vide.\n");
+    if(calcul_operations_temps(*operation_possible,liste_operation,nombre_operation,nombre_operation_possible,temps_cycle)==1)
         return;
-    }
-    while(fileDyn->premier != NULL) {
-        current = fileDyn->premier;
-        printf("Nom du spectacle : %s, Temps : %f\n", current->nom, current->temps);
-        fileDyn->premier = fileDyn->premier->suivant;
-        free(current->nom);
-        free(current);
-    }
+    return;
 }
-int main() {
-    printf("Hello, World!\n");
+
+int calcul_operations_temps(int* liste,operations_l** liste_operations,int nb_operations, int nombre_operations_possible,float temps_cyle){
+    float time = 0;
+    for(int i = 0;i<nombre_operations_possible;i++){
+        for(int j = 0;j<nb_operations;j++){
+            if(j = liste[i])
+                time+= (float) (*liste_operations)[j].temps;
+        }
+    }
+    if(time> temps_cyle)
+        return 1;
     return 0;
 }
