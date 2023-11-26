9b1f6196ab386a51d0e28ab925de0c967df6ad30
Victor Rouleau <victor.rouleau@edu.ece.fr>
2023-11-26 22:37:37 +0100

    commentaires precedence

--- test_file_Adam.c
+++ test_file_Adam.c
@@ -4,14 +4,14 @@
 int main(void){
     operations_l * liste_operation;
 
-    float temps_cycle = lecture_temps_cycle();
+    float temps_cycle = lecture_temps_cycle();  //lire le temps du cycle
     
     
     int nb_operation = lecture_fichier_operation(&liste_operation);
     
     precedence_init(nb_operation,&liste_operation);
     for(int i = 0;i<nb_operation;i++)
-        printf("Operation : %i, temps : %0.2f\n",liste_operation[i].operation,liste_operation[i].temps);
+        printf("Operation : %i, temps : %0.2f\n",liste_operation[i].operation,liste_operation[i].temps); //afficher le temps
     
     for(int i = 0;i<nb_operation;i++){
         for(int j = 0;j<liste_operation[i].nb_operation_precedente;j++){
