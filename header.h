30ba7c975ee6139d3ba66d536dd9ab84a64e5b43
S4m0S <adamnasreddine@outlook.com>
2023-12-01 22:59:37 +0100

    probleme avec nombre operation executé, fin proche

--- header.h
+++ header.h
@@ -6,11 +6,12 @@
 
 
 typedef struct {
-    int operation;
-    float temps;
-    int* op_precedent;
-    int nb_operation_precedente;
-    int effectuer;
+    int operation;                          // ID DE OPERATION
+    float temps;                            // TEMPS NECESSAIRE POUR EFFECTUER OPERATION
+    int* op_precedent;                      // LISTE DU NOMBRE OPERATION PRECEDENTE
+    int nb_operation_precedente;            // INDIQUE LE NOMBRE OPERATION PRECEDENTE NECCASSAIRE POUR EFFECTUER OPERATION
+    int effectuer;                          // INDIQUE SI OPERATION A ETE EFFECTUER, ELLE EST DEJA DANS UNE STATION
+    int effectuable;                        // INDIQUE SI OPERATION EST EFFECTUABLE, TOUTES LES OPERATIONS PRECEDENTES SONT DEJA EFFECTUER
 }operations_l;
 
 typedef struct
@@ -31,6 +32,11 @@ int lecture_fichier_operation(operations_l** liste_operation);
 float lecture_temps_cycle(void);
 int nb_ligne_fichier(char* nom_fichier);
 void precedence_init(int nombre_operation,operations_l** liste_operations);
+
+int check_operation_possible(operations_l ** liste_operation,int nb_operation);
+int comptage_operation(operations_l** liste_operation,int nb_operation);
+void implementation_Pert(operations_l** liste_operation, int nombre_operation);
+void associe_liste_operation_possible(operations_l** liste_operation,operations_l *** liste_operation_possible,int nb_operation);
 
 // Ouverture du fichier operation pour lecture des valeurs et inclusions dans une liste operation_l 
 int lecture_fichier_operation(operations_l** liste_operation){
@@ -54,6 +60,7 @@ int lecture_fichier_operation(operations_l** liste_operation){
         (*liste_operation)[operation_actuelle-1].operation = operation_acutelle;
         (*liste_operation)[operation_actuelle-1].temps = temps_actuelle;
         (*liste_operation)[operation_actuelle-1].effectuer = False;
+        (*liste_operation)[operation_actuelle-1].effectuable = False;
         operation_acutelle = 0;
         temps_actuelle = 0;
     }
@@ -105,7 +112,7 @@ void precedence_init(int nombre_operation,operations_l** liste_operations){
 
     precedent * liste_precedent = (precedent*) malloc(sizeof(precedent)* nb_arc);
     int actuelle = 0;
-    while(fscanf(fichier_prece,"%i %i\n",&liste_precedent[actuelle].op1,&liste_precedent[actuelle].op2)==2)
+    while(fscanf(fichier_prece,"%i %i\n",&liste_precedent[actuelle].op2,&liste_precedent[actuelle].op1)==2)
         actuelle++;
 
     int **liste_operation_precedente = (int**) malloc(sizeof(int*)*nombre_operation); 
@@ -130,6 +137,8 @@ void precedence_init(int nombre_operation,operations_l** liste_operations){
 
     for(int i = 0;i<nombre_operation;i++){
         (*liste_operations)[i].nb_operation_precedente = nb_operation_precedentes[i];
+        if(nb_operation_precedentes[i]==0)
+            (*liste_operations)[i].effectuable = True;
         (*liste_operations)[i].op_precedent = malloc(sizeof(int)*nb_operation_precedentes[i]);
         for(int j = 0;j<nb_operation_precedentes[i];j++){
             (*liste_operations)[i].op_precedent[j] = liste_operation_precedente[i][j];
@@ -145,76 +154,7 @@ void precedence_init(int nombre_operation,operations_l** liste_operations){
     free(nb_operation_precedentes);
 }
 
-
-void pert_algo(int nombre_operations, operations_l **liste_operations,float temps_cycle){ // ici on va essayer d'implementer la methode de Pert qui ressemble beaucoup à notre cas d'usage
-    int** liste_operations_effectuer = (int**) malloc(sizeof(int*)*nombre_operations);     //liste des operations qui ont deja ete effectue par cycle, quand i = 0 alors c'est le premier cycle toutes les operations effectue
-    for(int i = 0;i<nombre_operations;i++){
-        liste_operations_effectuer[i] = (int*) malloc(sizeof(int)*nombre_operations);
-    }
-    int * operations_effectuer = (int*) malloc(sizeof(int)*nombre_operations);           // cette liste nous permet d'acceder plus rapidement et plus facilement a toutes les operations effectuer sans avoir a faire un balayge en 2D
-    int* operations_facile = (int*) malloc(sizeof(int) * nombre_operations);            //liste des operations consideré comme facile à effectue car elle ne requiert aucunes autre tache pour etre effectue
-    int operations_trie_precedence = (int) malloc(sizeof(int)*nombre_operations);     // liste qui contient les operations trie par ordre croissant de nombre de precedances, plus une operation à besoin de precedences plus elle sera mise vers le fin de la liste
-    int* operations_possible = (int*) malloc(sizeof(int)*nombre_operations);            //liste des operations pouvants etre effectue à un instant T
-    int operations_facile_index = -1;
-    for(int operation_actuelle=0;operation_actuelle<nombre_operations;operation_actuelle++){
-        if((*liste_operations)[operation_actuelle].nb_operation_precedente==0)
-            operations_facile[operations_facile_index++] = (*liste_operations)[operation_actuelle].operation;
-    }
-}
-
-void tri_ordre_operation(tuple** operations_trie, operations_l** liste_operations,int nombre_operations){
-    tuple provisoire;
-    for(int i = 0;i<nombre_operations;i++){
-        (*operations_trie)[i].sommet = (*liste_operations)[i].operation;
-        (*operations_trie)[i].nb = (*liste_operations)[i].nb_operation_precedente;
-    }
-    for(int i = 0; i<nombre_operations;i++){
-        for(int j = 0;j<nombre_operations;j++){
-            if((*operations_trie)[j].nb>(*operations_trie)[j+1].nb){
-                provisoire.nb = (*operations_trie)[j].nb;
-                provisoire.sommet = (*operations_trie)[j].sommet;
-                (*operations_trie)[j].nb = (*operations_trie)[j+1].nb;
-                (*operations_trie)[j].sommet = (*operations_trie)[j+1].sommet;
-                (*operations_trie)[j+1].nb = provisoire.nb;
-                (*operations_trie)[j+1].sommet = provisoire.sommet;
-
-            }
-        }
-    }
-}
-
-
-
-void update_operations_possible(int index_operation_facile,int **operation_possible, int nombre_operation, int * operation_effectuer, int * opeartions_facile, operations_l **liste_operation,float temps_cycle){  //cette fonction va nous permettre de mettre a jour notre liste contenant les operations effectuables à un instant T
-    int nombre_operation_possible = 0;
-    int possible = 1;
-    for(int i = 0;i<index_operation_facile;i++){
-        (*operation_possible)[i] = opeartions_facile[i];
-        nombre_operation_possible++;
-    }
-    //if(calcul_operations_temps(*operation_possible,liste_operation,nombre_operation,nombre_operation_possible,temps_cycle)==1)
-    //    return;
-    for(int operation_i = 0;operation_i<nombre_operation;operation_i++){
-        for(int operation_j=0;operation_j<(*liste_operation)[operation_i].nb_operation_precedente;operation_j++){
-            possible = 1;
-            for(int operation_effectue = 0; operation_effectue < sizeof(operation_effectuer)/sizeof(int);operation_effectue++){            // ca ne fonctionne pas mais c'est un debut 
-                if((*liste_operation)[operation_i].op_precedent[operation_j] != operation_effectuer[operation_effectue]){
-                    possible=1;
-                }
-            }
-            if(possible==1){
-                (*operation_possible)[nombre_operation_possible] = (*liste_operation)[operation_i].operation;
-                nombre_operation_possible++;
-            }
-        }
-    }
-    return;
-}
-
-
-
-
-
+/*
 float calcul_chemin_rapide(operations_l*** operation_effectuable, int nb_operation_effectuable){
     //int nb_operation_effectuable = (int) (sizeof(*operation_effectuable)/sizeof(operations_l*));
     if(nb_operation_effectuable==1)
@@ -246,6 +186,8 @@ float calcul_chemin_rapide(operations_l*** operation_effectuable, int nb_operati
     free(chemins_possible);
     return plus_rapide;
 }
+*/
+// CETTE FONCTION EST OPERATIONELLE MAIS NECESSITE UNE GRANDE PUISSANCE DE CALCUL, CEPENDANT ELLE PEUT DONNER DES RESULTATS PLUS SATISFAISANT
 
 
 float calcul_chemin_possible_rapide(operations_l*** operation_effectuable, int nb_operation_effectuable,float temps_actuel){
@@ -277,17 +219,61 @@ float calcul_chemin_possible_rapide(operations_l*** operation_effectuable, int n
 
 
 
-void implementation_Pert(operations_l** liste_operation);
+void implementation_Pert(operations_l** liste_operation, int nombre_operation){
+    int nb_operation_restante = comptage_operation(liste_operation,nombre_operation);
+    int nb_operation_possible = 0;
+    while(nb_operation_restante!=0){
+        nb_operation_possible = check_operation_possible(liste_operation,nombre_operation) ;
+        operations_l **liste_operation_possible = (operations_l**) malloc(sizeof(operations_l*)*nb_operation_possible);
+        associe_liste_operation_possible(liste_operation,&liste_operation_possible,nombre_operation);
+        calcul_chemin_possible_rapide(&liste_operation_possible,nb_operation_possible,0);
+        nb_operation_restante = comptage_operation(liste_operation,nombre_operation);
+        free(liste_operation_possible);
+    }
+    printf("OUI");
+}
+
+void associe_liste_operation_possible(operations_l** liste_operation,operations_l *** liste_operation_possible,int nb_operation){
+    for(int operation_i=0;operation_i<nb_operation;operation_i++){
+        if((*liste_operation)[operation_i].effectuable == True && (*liste_operation)[operation_i].effectuer == False){
+            (*liste_operation_possible)[operation_i] = &(*liste_operation)[operation_i];
+        }
+    }
+}
 
 
 
 
-
-int comptage_operation(operations_l** liste_operation,int nb_operation){
-    int nb_operation_restante= 0;
+int check_operation_possible(operations_l ** liste_operation,int nb_operation){    // PARCOURS LA LISTE DES OPERATIONS AFIN DE METTRE A JOUR LEURS ETATS
+    int actuelle_etat;
+    int nb_operation_effectuable= 0;
     for(int operation_i = 0;operation_i<nb_operation;operation_i++){
+        actuelle_etat = False;
+        if((*liste_operation)[operation_i].effectuer==False && (*liste_operation)[operation_i].effectuable==False){
+            actuelle_etat = True;
+            for(int op_precedente_i = 0;op_precedente_i<(*liste_operation)[operation_i].nb_operation_precedente;op_precedente_i++){
+                for(int operation_j = 0;operation_j<nb_operation;operation_j++){
+                    if((*liste_operation)[operation_i].op_precedent[op_precedente_i]==(*liste_operation)[operation_j].operation){
+                        if((*liste_operation)[operation_j].effectuer==False)
+                            actuelle_etat = False;
+                    }
+                }
+            }
+            if(actuelle_etat==True){
+                (*liste_operation)[operation_i].effectuable = True;
+                nb_operation_effectuable++;
+            }
+        }
+    }
+    return nb_operation_effectuable;
+}
+
+int comptage_operation(operations_l** liste_operation,int nb_operation){    // COMPTE LE NOMBRE OPERATION RESTANTE, LES OPERATIONS QUI N ONT PAS ENCORE ETE EXECUTE
+    int nb_operation_restante= 0;
+    for(int operation_i = 0;operation_i<nb_operation;operation_i++){        // POUR CHAQUE OPERATION, ON REGARDE SI ELLE A DEJA ETE EXECUTE OU NON
         if((*liste_operation)[operation_i].effectuer==False)
             nb_operation_restante++;
+        //printf("Etat : %i",(*liste_operation)[operation_i].effectuer);
     }
     return nb_operation_restante;
 }
--- test_file_Adam.c
+++ test_file_Adam.c
@@ -8,16 +8,12 @@ int main(void){
     
 
     int nb_operation = lecture_fichier_operation(&liste_operation);
-    operations_l ** liste_operations = malloc(sizeof(operations_l*)*nb_operation);
-
-    for(int i = 0;i<nb_operation;i++){
-        liste_operations[i] = &liste_operation[i];
-    }
 
     //float chemin_plus_rapide = calcul_chemin_rapide(&liste_operations, 10);
-    float autre_chemin = calcul_chemin_possible_rapide(&liste_operations,nb_operation,0);
+    //float autre_chemin = calcul_chemin_possible_rapide(&liste_operations,nb_operation,0);
     //printf("Chemin plus rapide %f \n",chemin_plus_rapide);
-    printf("Chemin plus rapide %f \n",autre_chemin);
+    //printf("Chemin plus rapide %f \n",autre_chemin);
+    implementation_Pert(&liste_operation,nb_operation);
     
     /*
     precedence_init(nb_operation,&liste_operation);
